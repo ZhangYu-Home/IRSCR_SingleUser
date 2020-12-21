8783a76db9f6e5da85f6e43c7f1f6f52b39d75df
ZhangYu <939476975@qq.com>
2020-12-21 15:32:28 +0800

    添加了对应于预编码矩阵求解的部分代码

--- getPrecodeMat.m
+++ getPrecodeMat.m
@@ -17,45 +17,57 @@ function precode_mat = getPrecodeMat(scene,g_AP_PU,g_AP_SUs,decode_mat,weight_ma
     end
 
     while(1)
+        %precode_mat_tmp为连续凸近似每一轮迭代的初始点,leak_pow_tmp为相应的近似干扰泄漏阈值
         precode_mat_tmp = precode_mat;
-        %% 情形一：功率约束为无效约束
+        leak_pow_tmp = scene.leak_pow;
+        for i = 1:scene.n_SU
+            leak_pow_tmp = leak_pow_tmp - real(trace(precode_mat_tmp(:,:,i)'*(Z_p-X_p)*precode_mat_tmp(:,:,i)));
+        end
+        
+       %% 情形一：功率约束为无效约束
         mu = 0;
-        mu_low = 0;
-        mu_up = 0;
         precode_mat = calPrecodeMat1(X_0,Z_p,mu,Y,X_p,precode_mat_tmp);
-        val_J = calInterferenceLeak(Z_p,X_p,precode_mat,precode_mat_tmp,scene.leak_pow);
-        if(val_J > 0)
-            incre_val = 0.1;
-            
+        val_J = calInterferenceLeak(Z_p,X_p,precode_mat,precode_mat_tmp,leak_pow_tmp);
+        if(val_J > 0)         
+            %利用指数步进确定mu的上界和下界
+            mu = 0.1;
             while(1)
-                precode_mat = calPrecodeMat1(X_0,Z_p,mu_low+incre_val,Y,X_p,precode_mat_tmp);
+                precode_mat = calPrecodeMat1(X_0,Z_p,mu,Y,X_p,precode_mat_tmp);
                 val_J = calInterferenceLeak(Z_p,X_p,precode_mat,precode_mat_tmp,scene.leak_pow);
-                if(val_J > 0)
-                    mu_low = mu_low+incre_val;
-                    incre_val = incre_val * 2;
-                else
-                    mu_up = mu_low+incre_val;
-                    break;
-                end
+                if(val_J > 0) mu = mu * 2; else break; end
             end
+            mu_low = mu/2;
+            mu_up = mu;
+            
+            %基于求得的上界和下界，利用二分搜索求解上界和下界
             while(1)
                 mu = (mu_low + mu_up)/2;
                 precode_mat = calPrecodeMat1(X_0,Z_p,mu,Y,X_p,precode_mat_tmp);
                 val_J = calInterferenceLeak(Z_p,X_p,precode_mat,precode_mat_tmp,scene.leak_pow);
                 if(val_J > 0) mu_low = mu; else mu_up = mu; end
                 if(mu_up - mu_low < 0.001)
+                    %取mu的下界，保证约束条件满足
                     precode_mat = calPrecodeMat1(X_0,Z_p,mu_low,Y,X_p,precode_mat_tmp);
                     break;
                 end
             end
         end
         
-        %% 判断功率约束是否为有效约束
-        if(isActivePowerCon(scene,precode_mat))
-            disp('有效约束');
+        %% 判断是否满足功率约束，如果满足，则一轮求解结束，进入下一轮，否则考虑情形二
+        if(isSatifyPowerCon(scene,precode_mat))
+            continue;
         end
 
         %% 情形二：功率约束为有效约束
+        leak_pow_tmp = max(eig(X_p))*scene.max_pow - leak_pow_tmp;
+        lambda = 0;
+        mu = 0;
+        precode_mat = calPrecodeMat2(X_0,Z_p,lambda,mu,Y,X_p,precode_mat_tmp);
+        if(isSatifyPowerCon(scene,precode_mat))
+            
+        end
+        
+        
         while(1)
             
         end 
@@ -85,27 +97,45 @@ function precode_mat = calPrecodeMat1(X_0,Z_p,mu,Y,X_p,precode_mat)
 end
 
 %% 在情形一中，当mu变化时，计算干扰泄漏约束
-function val_J = calInterferenceLeak(Z_p,X_p,precode_mat,precode_mat_tmp,leak_pow)
+function val_J = calInterferenceLeak1(Z_p,X_p,precode_mat,precode_mat_tmp,leak_pow_tmp)
     n_SU = size(precode_mat,3);
     val_J = 0;
     for i = 1:n_SU
         val_J = val_J + real(trace(precode_mat(:,:,i)'*Z_p*precode_mat(:,:,i)));
         val_J = val_J - 2*real(trace(precode_mat_tmp(:,:,i)'*(Z_p-X_p)*precode_mat(:,:,i)));
-        val_J = val_J + real(trace(precode_mat_tmp(:,:,i)'*(Z_p-X_p)*precode_mat_tmp(:,:,i)));
     end
-    val_J = val_J - leak_pow;
+    val_J = val_J - leak_pow_tmp;
 end
 
-%% 判断功率约束是否为积极约束
-function Flag = isActivePowerCon(scene,precode_mat)
+%% 判断是否满足功率约束
+function Flag = isSatifyPowerCon(scene,precode_mat)
     sum_pow = 0;
     for i = 1:scene.n_SU
         sum_pow = sum_pow + trace(precode_mat(:,:,i)'*precode_mat(:,:,i));
     end
-    Flag = scene.max_pow - sum_pow > 1e-3;
+    Flag = scene.max_pow - sum_pow >= 0;
 end
 
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%情形二相关函数%%%%%%%%%%%%%%%%%%%%%% 
+%% 在情形二中，当mu和lambda变化时，计算预编码矩阵时用到的函数
+function precode_mat = calPrecodeMat2(X_0,Z_p,lambda,mu,Y,X_p,precode_mat)
+    n_SU = size(precode_mat,3);
+    tmp_coeff1 = inv(X_0+lambda*eye(n_SU));
+    tmp_coeff2 = mu*(Z_p - X_p);
+    for i = 1:n_SU
+        precode_mat(:,:,i) = tmp_coeff1*(Y(:,:,i)-tmp_coeff2*precode_mat(:,:,i));
+    end
+end
+
+%% 在情形一中，当mu变化时，计算干扰泄漏约束
+function val_J = calInterferenceLeak2(Z_p,X_p,precode_mat,precode_mat_tmp,leak_pow_tmp)
+    n_SU = size(precode_mat,3);
+    val_J = 0;
+    for i = 1:n_SU
+        val_J = 2*real(trace(precode_mat_tmp(:,:,i)'*(Z_p-X_p)*precode_mat(:,:,i)));
+    end
+    val_J = val_J - leak_pow_tmp;
+end
 %% 计算mu值
 function mu = calMu2()
     mu = 0;
