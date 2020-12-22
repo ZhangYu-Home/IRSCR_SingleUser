c0321ff8f4faace75dc936afaffd35e722b0d09f
ZhangYu <939476975@qq.com>
2020-12-22 16:45:30 +0800

    完善了主函数中的求解流程

--- Main.m
+++ Main.m
@@ -11,6 +11,8 @@ func = normalFuncSet; %导入函数集
 n_monte = 2; %蒙特卡洛仿真次数
 n_pow = 10; %功率迭代的次数     
 [scene,dist] = func.init();%初始化场景参数
+scene
+dist
 
 %% 进行蒙特卡洛仿真
 tic
@@ -20,24 +22,37 @@ for cnt_monte = 1:n_monte
     for cnt_pow = 1:n_pow
         scene.max_pow = 0.2*cnt_pow;
         %disp(['    The max Power is ', num2str(scene.max_pow),'W.']);
+        %% 初始化过程
         %初始化反射系数矩阵和预编码矩阵
-        [precode_mat,reflect_mat] = func.initPrecodeAndReflectMat(scene);       
+        [precode_mat,reflect_mat] = func.initPrecodeAndReflectMat(scene);
+        
         %计算联合信道
         [g_AP_PU,g_AP_SUs] = func.getUnionChannel(channel,reflect_mat);
         %计算每个次级用户对应的功率矩阵和干扰协方差矩阵
         [sig_mat,jam_mat] = func.getSigAndJamMat(g_AP_SUs,precode_mat,scene.noise_SU);
-        %计算解码矩阵和辅助矩阵
-        [decode_mat,weight_mat] = getDecodeAndWeightMat(sig_mat,jam_mat,g_AP_SUs,precode_mat);
-        %precode_mat
-        
-        precode_mat = getPrecodeMat(scene,g_AP_PU,g_AP_SUs,decode_mat,weight_mat,precode_mat);
-        %precode_mat
-        %计算每个次级用户对应的功率矩阵和干扰协方差矩阵
-        [sig_mat,jam_mat] = func.getSigAndJamMat(g_AP_SUs,precode_mat,scene.noise_SU);
-        %% 计算所有次级用户的速率和
+        %计算所有次级用户的速率和
         sum_rate = func.getWeightSumRate(sig_mat,jam_mat);
-        disp(['        The sum of rates is ',num2str(sum_rate),' bps.']);
         
+        %% 基于交替优化求解问题
+        while(1)
+            sum_rate_tmp = sum_rate;
+            %计算解码矩阵和辅助矩阵
+            [decode_mat,weight_mat] = getDecodeAndWeightMat(sig_mat,jam_mat,g_AP_SUs,precode_mat);
+            %给定其他参数的情况下计算预编码矩阵
+            precode_mat = getPrecodeMat(scene,g_AP_PU,g_AP_SUs,decode_mat,weight_mat,precode_mat);
+            %给定其他参数的情况下计算反射系数矩阵
+            reflect_mat = getReflectMat(scene,channel,precode_mat,reflect_mat);          
+            %计算联合信道
+            [g_AP_PU,g_AP_SUs] = func.getUnionChannel(channel,reflect_mat);
+            %计算每个次级用户对应的功率矩阵和干扰协方差矩阵
+            [sig_mat,jam_mat] = func.getSigAndJamMat(g_AP_SUs,precode_mat,scene.noise_SU);
+            %计算所有次级用户的速率和
+            sum_rate = func.getWeightSumRate(sig_mat,jam_mat);
+            disp(['        The sum of rates is ',num2str(sum_rate),' bps.']);
+            if(abs(sum_rate - sum_rate_tmp) < 0.001)
+                break;
+            end
+        end 
     end
 end
 toc
