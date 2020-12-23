47aa4d5163f567782ac2496894547a1e8c055ad7
ZhangYu <939476975@qq.com>
2020-12-23 17:12:53 +0800

    当前版本利用和速率作为终止条件

--- getPrecodeMat.m
+++ getPrecodeMat.m
@@ -23,43 +23,35 @@ function precode_mat = getPrecodeMat(scene,g_AP_PU,g_AP_SUs,decode_mat,weight_ma
         for i = 1:scene.n_SU
             leak_pow_tmp = leak_pow_tmp - real(trace(precode_mat_tmp(:,:,i)'*(Z_p-X_p)*precode_mat_tmp(:,:,i)));
         end
+        disp(['干扰泄漏约束 leak_pow_tmp = ',num2str(leak_pow_tmp)]);
         
        %% 情形一：功率约束为无效约束
         mu = 0;
         precode_mat = calPrecodeMat1(X_0,Z_p,mu,Y,X_p,precode_mat_tmp);
         val_J = calInterferenceLeak1(Z_p,X_p,precode_mat,precode_mat_tmp);
         if(val_J > leak_pow_tmp)         
-            %利用指数步进确定mu的上界和下界
-            mu = 0.1;
-            while(1)
-                disp('情形一')
-                pause;
-                precode_mat = calPrecodeMat1(X_0,Z_p,mu,Y,X_p,precode_mat_tmp);
-                val_J = calInterferenceLeak1(Z_p,X_p,precode_mat,precode_mat_tmp);
-                if(val_J > leak_pow_tmp) mu = mu * 2; else break; end
-            end
+            %利用指数步进确定mu的上界
             mu_low = 0;
-            mu_up = mu;
+            mu_up = 0.1;
+            while(val_J > leak_pow_tmp)
+                mu_up = mu_up * 2;
+                precode_mat = calPrecodeMat1(X_0,Z_p,mu_up,Y,X_p,precode_mat_tmp);
+                val_J = calInterferenceLeak1(Z_p,X_p,precode_mat,precode_mat_tmp);
+            end
             
             %基于求得的上界和下界，利用二分搜索求解上界和下界
-            disp('二分搜索');
-            while(1)
+            while(mu_up - mu_low > 0.001)
                 mu = (mu_low + mu_up)/2;
                 precode_mat = calPrecodeMat1(X_0,Z_p,mu,Y,X_p,precode_mat_tmp);
                 val_J = calInterferenceLeak1(Z_p,X_p,precode_mat,precode_mat_tmp);
                 if(val_J > leak_pow_tmp) mu_low = mu; else mu_up = mu; end
-                if(mu_up - mu_low < 0.001)
-                    %取mu的下界，保证约束条件满足
-                    precode_mat = calPrecodeMat1(X_0,Z_p,mu_up,Y,X_p,precode_mat_tmp);
-                    break;
-                end
             end
+            %取mu的上界，保证约束条件满足
+            precode_mat = calPrecodeMat1(X_0,Z_p,mu_up,Y,X_p,precode_mat_tmp);
         end
         
         %% 判断是否满足功率约束，如果不满足，考虑情形二
         if(calTotalPower(precode_mat) > scene.max_pow)
-            disp('情形二')
-            pause;
             %% 情形二：功率约束为有效约束
             leak_pow_tmp = max(eig(X_p))*scene.max_pow - leak_pow_tmp;
             lambda = 0;
@@ -72,21 +64,16 @@ function precode_mat = getPrecodeMat(scene,g_AP_PU,g_AP_SUs,decode_mat,weight_ma
                 %如果功率约束条件不满足，则继续计算
                 if(calTotalPower(precode_mat) > scene.max_pow)
                     %利用指数步进寻找lambda的上界
-                    lambda = 0.1;
-                    while(1)
-                        mu = calMu2(leak_pow_tmp,X_0,Z_p,X_p,Y,precode_mat,precode_mat_tmp,lambda);
-                        precode_mat = calPrecodeMat2(X_0,Z_p,lambda,mu,Y,X_p,precode_mat_tmp);
-                        if(calTotalPower(precode_mat) > scene.max_pow)
-                            lambda = lambda*2;
-                        else
-                            break;
-                        end
-                    end
                     lambda_low = 0;
-                    lambda_up = lambda;
+                    lambda_up = 0.1;
+                    while(calTotalPower(precode_mat) > scene.max_pow)
+                        lambda_up = lambda_up*2;
+                        mu = calMu2(leak_pow_tmp,X_0,Z_p,X_p,Y,precode_mat,precode_mat_tmp,lambda_up);
+                        precode_mat = calPrecodeMat2(X_0,Z_p,lambda_up,mu,Y,X_p,precode_mat_tmp);
+                    end
                     
                     %利用二分搜索求解最优lambda
-                    while(1)
+                    while(lambda_up-lambda_low > 0.001)
                         lambda = (lambda_up + lambda_low)/2;
                         mu = calMu2(leak_pow_tmp,X_0,Z_p,X_p,Y,precode_mat,precode_mat_tmp,lambda);
                         precode_mat = calPrecodeMat2(X_0,Z_p,lambda,mu,Y,X_p,precode_mat_tmp);
@@ -94,15 +81,11 @@ function precode_mat = getPrecodeMat(scene,g_AP_PU,g_AP_SUs,decode_mat,weight_ma
                         if(val_P > scene.max_pow) 
                             lambda_up = lambda; 
                         else
-                            lambda_low = lambda; 
-                        end
-                        if(lambda_up-lambda_low < 0.001)
-                            lambda = lambda_up;
-                            mu = calMu2(leak_pow_tmp,X_0,Z_p,X_p,Y,precode_mat,precode_mat_tmp,lambda);
-                            precode_mat = calPrecodeMat2(X_0,Z_p,lambda,mu,Y,X_p,precode_mat_tmp);
-                            break;
+                            lambda_low = lambda;
                         end
                     end
+                    mu = calMu2(leak_pow_tmp,X_0,Z_p,X_p,Y,precode_mat,precode_mat_tmp,lambda_up);
+                    precode_mat = calPrecodeMat2(X_0,Z_p,lambda_up,mu,Y,X_p,precode_mat_tmp);
                 end 
             end
         end
@@ -116,8 +99,6 @@ function precode_mat = getPrecodeMat(scene,g_AP_PU,g_AP_SUs,decode_mat,weight_ma
         sum_rate = func.getWeightSumRate(sig_mat,jam_mat);
         
         if(abs(sum_rate-sum_rate_tmp) < 1e-6)
-            disp(['mu_up = ',num2str(mu_up)]);
-            disp(['mu_low = ',num2str(mu_low)]);
             break;
         end
     end
@@ -130,7 +111,7 @@ function precode_mat = calPrecodeMat1(X_0,Z_p,mu,Y,X_p,precode_mat_tmp)
     mat_coeff2 = mu*(Z_p - X_p);
     precode_mat = zeros(size(precode_mat_tmp));
     for i = 1:n_SU
-        precode_mat(:,:,i) = mat_coeff1*(Y(:,:,i)'-mat_coeff2*precode_mat_tmp(:,:,i));
+        precode_mat(:,:,i) = mat_coeff1*(Y(:,:,i)'+mat_coeff2*precode_mat_tmp(:,:,i));
     end
 end
 
@@ -158,7 +139,7 @@ function precode_mat = calPrecodeMat2(X_0,Z_p,lambda,mu,Y,X_p,precode_mat)
     tmp_coeff1 = inv(X_0+lambda*eye(size(X_0,1)));
     tmp_coeff2 = mu*(Z_p - X_p);
     for i = 1:size(precode_mat,3)
-        precode_mat(:,:,i) = tmp_coeff1*(Y(:,:,i)-tmp_coeff2*precode_mat(:,:,i));
+        precode_mat(:,:,i) = tmp_coeff1*(Y(:,:,i)'+tmp_coeff2*precode_mat(:,:,i));
     end
 end
 
@@ -169,7 +150,7 @@ function val_J = calInterferenceLeak2(Z_p,X_p,precode_mat,precode_mat_tmp)
         val_J = 2*real(trace(precode_mat_tmp(:,:,i)'*(Z_p-X_p)*precode_mat(:,:,i)));
     end
 end
-%% 计算mu值
+%% 在情形二中，给定lamdba下，计算mu值
 function mu = calMu2(leak_pow_tmp,X_0,Z_p,X_p,Y,precode_mat,precode_mat_tmp,lambda)
     n_SU = size(precode_mat,3);
     mat_coe1 = (Z_p-X_p)*inv(X_0+lambda*eye(size(X_0,1)));
@@ -184,4 +165,14 @@ function mu = calMu2(leak_pow_tmp,X_0,Z_p,X_p,Y,precode_mat,precode_mat_tmp,lamb
     end
 
     mu = tmp_coeff1/tmp_coeff2;
+end
+
+%% 计算目标函数值
+function val_obj = calValObjFunc(X_0,Y,precode_mat)
+    val_obj = 0;
+    for i = 1:size(precode_mat,3)
+        val_obj = val_obj + real(trace(precode_mat(:,:,i)'*X_0*precode_mat(:,:,i)));
+        val_obj = val_obj - real(trace(Y(:,:,i)*precode_mat(:,:,i)));
+        val_obj = val_obj - real(trace(Y(:,:,i)'*precode_mat(:,:,i)'));
+    end
 end
